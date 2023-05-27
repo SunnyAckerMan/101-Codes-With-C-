b17ae5001b0a0eed0b89290618b02d9f86419dcf
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-27 11:38:22 +0600

    Version 2

---
+++ 53_User_Authentication/53_User_Authentication/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Simulated user credentials for authentication
+        string username = "admin";
+        string password = "@admin99";
+
+        Console.WriteLine("Enter username:");
+        string inputUsername = Console.ReadLine();
+
+        Console.WriteLine("Enter password:");
+        string inputPassword = Console.ReadLine();
+
+        if (inputUsername == username && inputPassword == password)
+        {
+            Console.WriteLine("Authentication successful! Welcome, " + username + ".");
+        }
+        else
+        {
+            Console.WriteLine("Authentication failed. Invalid username or password.");
+        }
+
+        Console.ReadLine();
+    }
+}
