d05cc9b554aaea447bbaf4b0255a9416499eb2b2
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-27 15:17:42 +0600

    Version 2

---
+++ 60_LCM/60_LCM/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+
+    static int LCM(int a, int b)
+    {
+        int gcd = GCD(a, b);
+        return (a * b) / gcd;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter two numbers:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int lcm = LCM(num1, num2);
+
+        Console.WriteLine("LCM: " + lcm);
+
+        Console.ReadLine();
+    }
+}
