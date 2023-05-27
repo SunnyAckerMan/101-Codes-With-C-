b514f5b38d5e77387d529c93efb746dc98ce26e8
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-27 10:59:57 +0600

    Version 1

---
+++ 51_Genarate_100_RegistrationNumber/51_Genarate_100_RegistrationNumber/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Student
+{
+    private string name;
+    private string registerNumber;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string RegisterNumber
+    {
+        get { return registerNumber; }
+    }
+
+    private static int count = 1000;
+
+    static Student ()
+    {
+        Console.WriteLine("Generating register numbers...");
+    }
+
+    public Student(string name)
+    {
+        this.name = name;
+        count++;
+        registerNumber = "R" + count.ToString();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student[] students = new Student[100];
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            students[i] = new Student("Student " + (i + 1));
+        }
+        Console.WriteLine("Register number Generated successfully");
+
+        foreach (Student student in students)
+        {
+            Console.WriteLine("Name: " + student.Name + "\tRegister Number: " + student.RegisterNumber);
+        }
+
+        Console.WriteLine();
+    }
+}
