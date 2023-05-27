ff749bf0de1e688e54d87166c0d2086b23ad6f02
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-27 15:11:52 +0600

    Version 2

---
+++ 59_GCD/59_GCD/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Enter two numbers:");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int gcd = GCD(num1, num2);
+
+        Console.WriteLine("GCD: " + gcd);
+
+        Console.ReadLine();
+    }
+}
