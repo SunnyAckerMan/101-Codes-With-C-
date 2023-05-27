46cc29a81a4eb3a0788ed55a61706a93b9e3bc5d
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-27 11:59:21 +0600

    Version 2

---
+++ 54_Currency_Converter/54_Currency_Converter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class CurrencyConverter
+{
+    private double exchangeRate;
+
+    public CurrencyConverter(double exchangeRate)
+    {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public double ConvertToUSD(double amount)
+    {
+        return amount / exchangeRate;
+    }
+
+    public double ConvertFromUSD(double amount)
+    {
+        return amount * exchangeRate;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Create a currency converter with an exchange rate of 107.20 (1 USD = 107.20 BDT)
+        CurrencyConverter converter = new CurrencyConverter(107.20);
+
+        Console.WriteLine("Enter amount in USD: ");
+        double usdAmount = Convert.ToDouble(Console.ReadLine());
+
+        double convertedAmount = converter.ConvertFromUSD(usdAmount);
+
+        Console.WriteLine("Converted amount: " + convertedAmount + " BDT");
+
+        Console.ReadLine();
+    }
+}
