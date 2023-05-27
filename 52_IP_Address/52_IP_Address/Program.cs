933233f5f9a0ce898a4b1ee847d5bfd8d9aa84ea
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-27 11:26:42 +0600

    Version 1

---
+++ 52_IP_Address/52_IP_Address/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        string hostName = Dns.GetHostName(); // Get the host name of the machine
+        IPHostEntry hostEntry = Dns.GetHostEntry(hostName); // Get the IP addresses associated with the host name
+
+        foreach (IPAddress ipAddress in hostEntry.AddressList)
+        {
+            if (ipAddress.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+            {
+                Console.WriteLine("IP Address: " + ipAddress.ToString());
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
