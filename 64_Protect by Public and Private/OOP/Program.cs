fd95cb6e891ff20ff83764dc5ea27854b143df87
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-29 15:52:19 +0600

    Version 2

---
+++ 64_Protect by Public and Private/OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using OOP;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student(19, "Sunny", 89); //Class Instance Creation
+        student.setName("Kaiser");
+        Console.WriteLine(student.getName());
+
+        student.setGrade(69);
+        Console.WriteLine(student.getGrade());
+       
+
+
+    }
+}
---
+++ 64_Protect by Public and Private/OOP/Student.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Student
+    {
+        private int Age;
+        private string Name;
+        private float Grade;
+
+        public Student(int Age, string Name, float Grade)
+        {
+            this.Age = Age;
+            this.Name = Name;
+            this.Grade = Grade;
+        }
+
+        public Student(int Age, string Name) : this(Age, Name, 0)
+        {
+
+        }
+
+        public void sayHello() //Method
+        {
+            Console.WriteLine("Hello! {0}", Name);
+        }
+
+        public string getName()
+        {
+            if (Age >= 18)
+            {
+                return Name;
+            }
+            else
+            {
+                return "No Access!";
+            }
+        }
+
+        public void setName(string newName)
+        {
+            if (Name != "")
+            {
+                Name = newName;
+            }
+        }
+
+        public int getAge()
+        {
+            if (Age >= 18)
+            {
+                return Age;
+            }
+            else
+            {
+                return -1;
+            }
+
+        }
+
+        public void setAge(int newAge)
+        {
+            if (newAge != 0)
+            {
+                Age = newAge;
+            }
+        }
+
+        public float getGrade()
+        {
+            return Grade;
+        }
+
+        public void setGrade(float newGrade)
+        {
+            if (newGrade < 65)
+            {
+                newGrade = 65;
+            }
+            else if(newGrade > 100)
+                    {
+                newGrade = 100;
+            }
+            Grade = newGrade;
+            
+        }
+
+
+    }
+}
