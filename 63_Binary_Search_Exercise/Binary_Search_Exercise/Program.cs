1dc0a16b1244475a73805e579f8a30e04f8b96f5
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-29 11:57:28 +0600

    Version 2

---
+++ 63_Binary_Search_Exercise/Binary_Search_Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿class Bsearch
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Binary Search-----");
+        int[] a = {10,14,16,20,18,25};
+        int num = int.Parse(Console.ReadLine());
+
+        int answer = Array.BinarySearch(a,num);
+
+        if(answer < 0 )
+        {
+            Console.WriteLine("Number Does not exist!");
+        }
+        else
+        {
+            Console.WriteLine("The number is in the array number {0}", answer);
+        }
+    }
+}
