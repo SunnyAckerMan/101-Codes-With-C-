beeed657c8f3494c17a1fe965c823203e0f92f50
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-30 13:37:56 +0600

    Hacker Rank

---
+++ 66_HR_Problem_1/66_HR_Problem_1/Program.cs
@@ -0,0 +1,35 @@
+﻿class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32 (Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse); //Take inputs in a array format
+
+        int postitive = 0;
+        int negetive = 0;
+        int zero = 0;
+
+        foreach (int i in arr) //Check all inputs in the array and icress tham
+        {
+            if (i > 0)
+            {
+                postitive++;
+            }
+            else if (i < 0)
+            {
+                negetive++;
+            }
+            else 
+                zero++;
+        }
+
+        decimal positiveRatio = (decimal)postitive / n;
+        decimal negetiveCount = (decimal)negetive / n;
+        decimal zeroCount = (decimal)zero /n;
+
+        Console.WriteLine(positiveRatio.ToString("F6"));
+        Console.WriteLine(negetiveCount.ToString("F6"));
+        Console.WriteLine(zeroCount.ToString("F6"));
+    }
+}
