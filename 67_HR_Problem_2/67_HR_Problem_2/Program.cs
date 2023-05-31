3280eed57f0e9167088b53184f0014fb3e61c3d7
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-31 12:36:05 +0600

    Hacker Rank

---
+++ 67_HR_Problem_2/67_HR_Problem_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string[] arrTemp = Console.ReadLine().Split(' ');
+        int[] arr = Array.ConvertAll(arrTemp, int.Parse);
+
+        Array.Sort(arr);
+
+        long minSum = 0;
+        long maxSum = 0;
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            minSum += arr[i];
+            maxSum += arr[arr.Length - 1 - i];
+        }
+
+        Console.WriteLine($"{minSum} {maxSum}");
+    }
+}
