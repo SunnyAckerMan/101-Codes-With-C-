3fdde7299c53bc3da3fb7ea993a37647cd78a952
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-05-31 16:38:18 +0600

    Hacker Rank

---
+++ 68_Operator_Overloading/68_Operator_Overloading/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _68_Operator_Overloading
+{
+    public class BankAccount
+    {
+        private double cash;
+        private string owner;
+
+        public BankAccount(double cash, string owner)
+        {
+            this.cash = cash;
+            this.owner = owner;
+        }
+
+        public double Cash
+        {
+            get
+            {
+                return cash;
+            }
+        }
+
+        public string Owner
+        {
+            get
+            {
+                return owner;
+            }
+        }
+
+        public static BankAccount operator + (BankAccount bank1, BankAccount bank2 )
+        {
+            double newCash = bank1.Cash + bank2.Cash;
+            string newOwner = bank1.Owner + "+" + bank2.Owner;
+
+            BankAccount newBankAccount = new BankAccount( newCash, newOwner);
+            return newBankAccount;
+        }
+       
+    }
+}
---
+++ 68_Operator_Overloading/68_Operator_Overloading/Program.cs
@@ -0,0 +1,16 @@
+﻿using _68_Operator_Overloading;
+using System.Data.SqlTypes;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount bank1 = new BankAccount(180,"Sunny");
+        BankAccount bank2 = new BankAccount(220,"Naim");
+
+        BankAccount bank3 = bank1 + bank2;
+        Console.WriteLine(bank3.Owner);
+        Console.WriteLine(bank3.Cash);
+    }
+}
