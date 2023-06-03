833bc1dcac0a3ede792d272c4109212a198463a3
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-03 11:22:50 +0600

    Version 2

---
+++ 69_Passing_ObjectsTo_Methods/69_Passing_ObjectsTo_Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using _69_Passing_ObjectsTo_Methods;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person p1 = new Person("Jessi", 30);
+        Console.WriteLine("{0},{1}", p1.Name, p1.Age);
+
+        ChangePerson(p1 );
+        Console.WriteLine("{0},{1}",p1.Name, p1.Age);
+
+
+        static void ChangePerson(Person p1)
+        {
+            p1.Name = "Jeff";
+            p1.Age = 67;
+        }
+    }
+}
