dcc11a40304ee73619ce29440a50367ea5c9eb72
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-04 13:33:53 +0600

    Version 2

---
+++ 70_Method_OverLoading/70_Method_OverLoading/MethodOverloading.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace _70_Method_OverLoading
+{
+    public class MethodOverloading
+    {
+       public void Add()
+        {
+            Console.Write("Enter First Number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second Number: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The Sum is: {a+b}");
+
+        }
+
+        public int Add(int x, int y)
+        {
+            return x+y;
+        }
+
+        public string Add(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            return a+b+c;
+        }
+    }
+}
---
+++ 70_Method_OverLoading/70_Method_OverLoading/Program.cs
@@ -0,0 +1,20 @@
+﻿using _70_Method_OverLoading;
+using System.Threading.Channels;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Method Overloading");
+
+        MethodOverloading calculator = new MethodOverloading();
+        calculator.Add();
+
+        Console.WriteLine("The Next Result of Overloading: " + calculator.Add(10, 20));
+        Console.WriteLine("The Next Result of Overloading with String: " + calculator.Add("Ahmed"," Sunny"));
+        Console.WriteLine("The Next Result of Overloading with three Parameters: " + calculator.Add(30, 40, 50));
+
+
+        
+    }
+}
