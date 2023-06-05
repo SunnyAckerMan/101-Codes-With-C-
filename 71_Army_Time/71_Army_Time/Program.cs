17f60d60115e615b6d373733a68c732219b88b82
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-05 13:03:17 +0600

    Hacker Rank

---
+++ 71_Army_Time/71_Army_Time/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter time in AM/PM format (e.g., 12:30PM):");
+        string inputTime = Console.ReadLine();
+
+        // Parse the input time string
+        if (DateTime.TryParse(inputTime, out DateTime time))
+        {
+            string militaryTime = time.ToString("HH:mm:ss");
+            Console.WriteLine("Military time: " + militaryTime);
+        }
+        else
+        {
+            Console.WriteLine("Invalid input format. Please provide a valid time in AM/PM format.");
+        }
+    }
+}
