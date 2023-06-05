e2c6ac146c33ac7e4de7ecc953a36e5e81f57037
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-05 20:15:05 +0600

    Version 1

---
+++ 72_ValueType_ReferenceType/72_ValueType_ReferenceType/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] arr = new int[] { 1, 2, 3 };
+int[] arr2 = new int[3];
+arr2 = arr;
+arr2[0] = 10;
+Console.WriteLine(arr[0]); 
---
+++ 72_ValueType_ReferenceType/72_ValueType_ReferenceType/UserDefineDataType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72_ValueType_ReferenceType
+{
+    public class UserDefineDataType
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
