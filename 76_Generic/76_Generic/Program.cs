6e97854fbe7a48e6fd73ef790c2e9740da27c916
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-06 14:09:24 +0600

    Version 2

---
+++ 76_Generic/76_Generic/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Creating a generic class instance with type parameter as int
+        MyGenericClass<int> intGenericClass = new MyGenericClass<int>(10);
+        int intValue = intGenericClass.GenericMethod(5);
+        Console.WriteLine("Returned value: " + intValue);
+
+        // Creating a generic class instance with type parameter as string
+        MyGenericClass<string> stringGenericClass = new MyGenericClass<string>("Hello");
+        string stringValue = stringGenericClass.GenericMethod("World");
+        Console.WriteLine("Returned value: " + stringValue);
+    }
+}
+
+public class MyGenericClass<T>
+{
+    private T genericMemberVariable;
+
+    public MyGenericClass(T value)
+    {
+        genericMemberVariable = value;
+    }
+
+    public T GenericMethod(T genericParameter)
+    {
+        Console.WriteLine("Parameter type: " + typeof(T).ToString());
+        Console.WriteLine("Parameter value: " + genericParameter);
+
+        T returnValue = genericMemberVariable;
+        Console.WriteLine("Generic member variable value: " + returnValue);
+
+        return returnValue;
+    }
+}
