4a93f85e8f83e3e258dc35137b4849b503d8842e
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-07 16:42:41 +0600

    Version 2

---
+++ 79_maximum_and_minimum_elements_array/79_maximum_and_minimum_elements_array/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine($"Enter Array Size: ");
+string sizeInput = Console.ReadLine();
+int  size = Convert.ToInt32( sizeInput );
+
+object[] arr = new object[size];
+
+for ( int i = 0; i < size; i++ )
+{
+    Console.WriteLine($"Enter Element for Position {i}: ");
+    string userInput = Console.ReadLine();
+    arr[i] = Convert.ToInt32( userInput );
+}
+
+int max = Convert.ToInt32(arr[0]);
+int min = Convert.ToInt32(arr[0]);
+
+
+for (int i = 1; i < size; i++)
+{
+    int currentValue = Convert.ToInt32(arr[i]);
+
+    if(currentValue > max)
+    {
+       max = currentValue;
+    }
+    else if(currentValue < min)
+    {
+        min = currentValue;
+    }
+}
+
+Console.WriteLine($"Maximum Value is: {max}");
+Console.WriteLine($"Minimum Value is {min}");
