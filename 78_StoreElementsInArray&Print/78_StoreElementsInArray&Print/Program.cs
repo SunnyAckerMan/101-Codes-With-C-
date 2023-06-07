f0ddb7048e30526f79d0ac85794a31d14a241b61
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-07 11:05:48 +0600

    Version 2

---
+++ 78_StoreElementsInArray&Print/78_StoreElementsInArray&Print/Program.cs
@@ -0,0 +1,19 @@
+﻿object[] arr = new object[10];
+
+for(int i = 0; i < arr.Length; i++)
+{
+    Console.Write($"Enter Element {i}: ");
+    string userInput = Console.ReadLine();
+    arr[i] = userInput;
+}
+
+Console.WriteLine();
+Console.WriteLine("___________Elements in the Index___________");
+Console.WriteLine();
+
+int index = 0;
+foreach (object element in arr)
+{
+    Console.WriteLine($"Element {index}: {element}");
+    index++;
+}
