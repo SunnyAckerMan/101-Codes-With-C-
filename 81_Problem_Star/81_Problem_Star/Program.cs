948afa54a384521c9d05ad4b822179f0c60711d5
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-19 16:13:07 +0600

    Version 1

---
+++ 81_Problem_Star/81_Problem_Star/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Star Printing");
+Console.WriteLine();
+
+for (int row = 8; row >= 1; --row)//row = 7654321
+{
+    for (int col = 1; col <= row; ++col) //column = 23456789 (exit in 9)
+    {
+        Console.Write("*");
+    }
+
+    Console.WriteLine();
+}
+
+//********
+//*******
+//******
+//*****
+//****
+//***
+//**
+//*
+
