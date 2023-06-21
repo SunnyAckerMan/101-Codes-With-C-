46c570d416c9b60c1adc9328fbfd5f8ad268c640
Ahmed Zubayer Sunny <sunnysas150@gmail.com>
2023-06-21 14:23:27 +0600

    version 2

---
+++ 83_Linear_Search/83_Linear_Search/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Enter the elements of the array:");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+Console.Write("Enter the target value to search: ");
+int target = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i] == target) 
+    {
+        Console.WriteLine($"{target} is inthe index number {i}");
+    }
+}
+
+
